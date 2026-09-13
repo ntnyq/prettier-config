dd6edf9fb7fa96d85b61f4a65883331d67e4b461
ntnyq <ntnyq13@gmail.com>
2026-09-13 14:00:16 +0800

    build: bump pnpm to v12

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   clean: true,
+  dts: true,
   entry: ['src/index.ts'],
-  platform: 'neutral',
-  dts: {
-    tsgo: true,
-  },
+  platform: 'node',
 })
